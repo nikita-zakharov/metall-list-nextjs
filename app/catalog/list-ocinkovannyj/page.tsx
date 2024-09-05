31ae722c30735eae54f86f2b083ad9d8e6bbccb3
NikitaZakharov <nikzhrv@yandex.ru>
2024-09-05 15:54:26 +0300

    test performance

--- app/catalog/list-ocinkovannyj/page.tsx
+++ app/catalog/list-ocinkovannyj/page.tsx
@@ -6,18 +6,18 @@ import listyOcinkPic1 from '@/public/images/listy-ocinkovannyj-1.jpeg'
 import listyOcinkPic2 from '@/public/images/listy-ocinkovannyj-2.jpeg'
 import listyOcinkPic3 from '@/public/images/listy-ocinkovannyj-3.jpeg'
 import listyOcinkPic4 from '@/public/images/listy-ocinkovannyj-4.jpeg'
+import listyOcinkSmall from '@/public/images/list-ocink-small.png'
 import LightGallery from 'lightgallery/react';
 import { nlmkSiteUrl } from "@/app/constants";
 
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation } from "swiper/modules";
 
-const productImagePath = '/images/list-ocink-small.png'
 const thinkness = [0.5, 0.7, 0.8, 0.9, 1.0, 1.2, 1.5, 2.0, 2.5, 3.0]
 
 export default function CatalogSheetPage() {
     return (<div>
-        <div style={{ background: 'url("/images/banner-list-ocinkovannyj.png")', backgroundSize: 'cover', backgroundRepeat: 'no-repeat' }} className="relative w-full py-16">
+        <div style={{}} className="relative w-full py-16">
             <div className="container">
                 <h1 className="text-white text-4xl">Лист оцинкованный</h1>
             </div>
@@ -45,7 +45,7 @@ export default function CatalogSheetPage() {
                                     <tr className="border-b text-center text-sm" key={item}>
                                         <td className="py-5">
                                             <div className="flex items-center gap-4 justify-center">
-                                                <img src={productImagePath} width="48" alt="Лист оцинкованный" />
+                                                <Image src={listyOcinkSmall} width="48" alt="Лист оцинкованный" />
                                                 <span>Лист оцинкованный {Number(item).toFixed(1)} мм</span>
                                             </div>
                                         </td>
